e40de0c7d9a4c98dc1c332366bd17e01ce8b5789
BomJin2 <89959092+BomJin2@users.noreply.github.com>
2023-05-29 05:16:33 +0900

    Delete MOMENTUM directory

--- MOMENTUM/js/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["back1.jpg","back2.jpg","back3.jpg","back4.png"];
-
-const ChosenImage = images[Math.floor(Math.random() * images.length)];
-
-document.body.style.backgroundImage = `url(img/${ChosenImage})`;
-
-document.body.style.backgroundSize = `cover`;
-
